28280516587ac4fa489bfca487fb83e06c67c358
Waleed Lugod <lugodwaleed@gmail.com>
2023-09-13 09:53:52 +0800

    fonts

--- src/js/category.js
+++ src/js/category.js
@@ -4,3 +4,8 @@ import { data } from "../assets/data/data";
 document.querySelector("#category").innerHTML =
   // prettier-ignore-end
   `<div>category</div>`;
+
+//   `<div style="width: 100%; height: 20rem; background: ${data.categories[0].card.gradient};">
+//     <p>category</p>
+//     <img src=${data.categories[0].card["background-image"]} alt="" />
+//   </div>`;
