d61fccf3c7c67dae41d3be1ed19852936f218a36
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-09-18 22:58:07 +0800

    created base template of category

--- src/js/category.js
+++ src/js/category.js
@@ -1,11 +1,23 @@
 import { data } from "../assets/data/data";
 
-// prettier-ignore-start
-document.querySelector("#category").innerHTML =
-  // prettier-ignore-end
-  `<div>category</div>`;
+const card = `
+<div id="card" style="width: 100%;
+background: ${data.categories[6].card.gradient}, 
+url(${data.categories[6].card["background-image"]}); 
+background-size: cover;">
+    <div id="category-text">
+        <div id="title"></div>
+        <div id="desc"></div>
+    </div>
+    <div id="category-img">
+        <div id="side"></div>
+    </div>
+</div>`;
 
-//   `<div style="width: 100%; height: 20rem; background: ${data.categories[0].card.gradient};">
-//     <p>category</p>
-//     <img src=${data.categories[0].card["background-image"]} alt="" />
-//   </div>`;
+const title = `<h1>${data.categories[6].category}</h1>`;
+const desc = `<p>LAST SEASON, the Company of Ateneo Dancers (CADs) Streetdance team attempted to distinguish themselves from competitiors by adding soft textures and urban choreography to their performance in...</p>`;
+const sideImg = `<img src=${data.categories[6].card["side-image"]}></img>`;
+document.querySelector("#category-card").innerHTML = card;
+document.querySelector("#title").innerHTML = title;
+document.querySelector("#desc").innerHTML = desc;
+document.querySelector("#side").innerHTML = sideImg;
