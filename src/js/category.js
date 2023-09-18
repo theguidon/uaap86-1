7fde7f6cbb007c59f076a4944cc0cc5bf8523451
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-09-19 05:16:15 +0800

    added dynamic text color and opacity for cards

--- src/js/category.js
+++ src/js/category.js
@@ -1,23 +1,38 @@
 import { data } from "../assets/data/data";
 
-const card = `
-<div id="card" style="width: 100%;
-background: ${data.categories[6].card.gradient}, 
-url(${data.categories[6].card["background-image"]}); 
-background-size: cover;">
-    <div id="category-text">
-        <div id="title"></div>
-        <div id="desc"></div>
-    </div>
-    <div id="category-img">
-        <div id="side"></div>
-    </div>
-</div>`;
+const categoryCardContainer = document.querySelector("#category-card");
 
-const title = `<h1>${data.categories[6].category}</h1>`;
-const desc = `<p>LAST SEASON, the Company of Ateneo Dancers (CADs) Streetdance team attempted to distinguish themselves from competitiors by adding soft textures and urban choreography to their performance in...</p>`;
-const sideImg = `<img src=${data.categories[6].card["side-image"]}></img>`;
-document.querySelector("#category-card").innerHTML = card;
-document.querySelector("#title").innerHTML = title;
-document.querySelector("#desc").innerHTML = desc;
-document.querySelector("#side").innerHTML = sideImg;
+const styleElement = document.createElement("style");
+document.head.appendChild(styleElement);
+
+for (let i = 0; i < data.categories.length; i++) {
+  const card = `
+    <div id="card" style="
+      width: 100%;
+      background: ${data.categories[i].card.gradient}, 
+      url(${data.categories[i].card["background-image"]}); 
+      background-size: cover;
+    ">
+      <div id="category-text">
+        <div id="title" class="card-title">${data.categories[i].category}</div>
+        <div id="desc">LAST SEASON, the Company of Ateneo Dancers (CADs) Streetdance team attempted to distinguish themselves from competitors by adding soft textures and urban choreography to their performance in...</div>
+      </div>
+      <div id="category-img">
+        <div id="side"><img src=${data.categories[i].card["side-image"]} alt="Category Image"></div>
+      </div>
+    </div>`;
+
+  categoryCardContainer.innerHTML += card;
+
+  styleElement.innerHTML += `
+    #card:nth-child(${i + 1}) {
+      opacity: ${data.categories[i].card.opacity};
+    }
+    #card:nth-child(${i + 1}):hover {
+      opacity: 1;
+    }
+    #card:nth-child(${i + 1}):hover #title {
+      color: ${data.categories[i].card["title-color"]};
+    }
+  `;
+}
