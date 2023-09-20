e4d0451fcc3eb4515c7f47d3ceb0171934994aee
Waleed Lugod <lugodwaleed@gmail.com>
2023-09-21 02:40:36 +0800

    add open side nav

--- src/js/side-nav.js
+++ src/js/side-nav.js
@@ -1,6 +1,11 @@
 const sideNav = document.querySelector("#side-nav");
 const exitButton = document.querySelector("#side-nav .exit");
+const openButton = document.querySelector("#landing .open");
 
 exitButton.addEventListener("click", () => {
   sideNav.dataset.active = "false";
+});
+
+openButton.addEventListener("click", () => {
+  sideNav.dataset.active = "true";
 });
