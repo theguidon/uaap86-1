bc903aba7f4631f89ac905e192f5ca463d00eca0
Waleed Lugod <lugodwaleed@gmail.con>
2024-02-18 22:56:22 +0800

    adjust spacing

--- src/js/change.js
+++ src/js/change.js
@@ -1,17 +1,23 @@
 const player = document.querySelector("#landing-player");
+const playerOther = document.querySelector("#landing-player-other");
 
 let i = 0;
+let nextView = false;
 function change() {
 	i = (i + 1) % 11;
-	player.src =`/landing/${i}.png`;
-}
 
-function fade() {
-	player.style.opacity = 0;
-	setTimeout(function t() {
-		change()
+	if (nextView) {
+		player.src =`/landing/${i}.png`;
 		player.style.opacity = 1;
-	}, 400);
+		playerOther.style.opacity = 0;
+
+	} else {
+		playerOther.src =`/landing/${i}.png`;
+		player.style.opacity = 0;
+		playerOther.style.opacity = 1;
+	}
+
+	nextView = !nextView
 }
 
-setInterval(fade, 4000);
+setInterval(change, 2000);
