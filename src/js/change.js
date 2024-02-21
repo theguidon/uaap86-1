9d28d8a094f88f4eea2823915e85e76a25339971
Waleed Lugod <lugodwaleed@gmail.con>
2024-02-21 16:39:12 +0800

    fix upload bugs

--- src/js/change.js
+++ src/js/change.js
@@ -1,20 +1,22 @@
 const player = document.querySelector("#landing-player");
 const playerOther = document.querySelector("#landing-player-other");
 
+const basePath = "/uaap-86-second-semester-primer"
+
 let i = 0;
 let nextView = false;
 function change() {
 	i = (i + 1) % 11;
 
 	if (nextView) {
-		player.src =`/landing/${i}.png`;
+		player.src =`${basePath}/landing/${i}.png`;
 		playerOther.style.opacity = 0;
 		setTimeout(() => {
 			player.style.opacity = 1;
 		}, 200)
 
 	} else {
-		playerOther.src =`/landing/${i}.png`;
+		playerOther.src =`${basePath}/landing/${i}.png`;
 		player.style.opacity = 0;
 		setTimeout(() => {
 			playerOther.style.opacity = 1;
